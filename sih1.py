587ee7ec4ecd1e81f539f946c867164fe4ad07af
deep-wind <78983104+deep-wind@users.noreply.github.com>
2022-03-09 01:21:22 +0530

    Update sih1.py

--- sih1.py
+++ sih1.py
@@ -16,7 +16,7 @@ from tensorflow.keras.preprocessing import image
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
+#import cv2
 import numpy as np
 from tensorflow.keras.preprocessing import image
 import pathlib
