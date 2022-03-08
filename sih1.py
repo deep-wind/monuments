31cc0a1775c14f86dd0026209ed46e7c1ce4de45
deep-wind <78983104+deep-wind@users.noreply.github.com>
2022-03-09 01:38:13 +0530

    Update sih1.py

--- sih1.py
+++ sih1.py
@@ -18,7 +18,7 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 #Load dataset
-data_dir = pathlib.Path("monuments/")
+data_dir = pathlib.Path("monuments")
 image_count = len(list(data_dir.glob('*/*.png')))
 print(image_count)
 
